f19af3dfecb786db109046c7a2fb7f9174b8034f
udayteja27 <udayteja.kut@gmail.com>
2024-06-27 09:01:02 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ChartComponent from "./components/ChartComponent";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: "rgb(98, 98, 185)" }}>Charting Application</h1>
+      <ChartComponent />
     </div>
   );
-}
+};
 
 export default App;
